5de52c69b187d95835cc659cd448bef298e05fea
Kevin Lopes Costa <kevinlopes3012@gmail.com>
2025-09-13 11:47:45 -0300

    feat: inicia back-end & rotas get e post do user

---
+++ back-end/config/db.js
@@ -0,0 +1,17 @@
+import "dotenv/config"
+import mongoose from "mongoose";
+
+const { MONGO_URL } = process.env;
+
+export const connectDb = async ()=>{
+    try{
+    await mongoose.connect(MONGO_URL)
+        console.log('Conectado com o banco')
+    }catch(error){
+        console.log('Não conectou com o banco!', error)
+    }
+}
+
+
+
+
---
+++ back-end/domains/users/routes.js
@@ -0,0 +1,39 @@
+import { connectDb } from "../../config/db.js";
+import User from "./models.js";
+import { Router } from "express";
+import bcrypt from "bcryptjs";
+
+const router = Router();
+const bcryptSalt = bcrypt.genSaltSync();
+
+router.get('/', async (req, res)=>{
+    connectDb()
+    try {
+       const userDoc = await User.find(); 
+       
+        res.json(userDoc)
+    } catch (error) {
+        res.status(404).json(error);
+    }
+})
+
+router.post('/', async (req,res)=>{
+    connectDb();
+    const {name, email, password} = req.body
+
+    const encryptedPassword = bcrypt.hashSync(password, bcryptSalt)
+    
+    try {
+        const newUserDoc = await User.create({
+        name,
+        email,
+        password: encryptedPassword,
+    })    
+        res.json(newUserDoc)
+
+    } catch (error) {
+        res.status(404).json(error);
+    }
+})
+
+export default router;
